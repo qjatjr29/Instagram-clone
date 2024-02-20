2ed451bb10ad4b7aad7ef07b5ee6191580eed932
beomsic <qjatjr29@khu.ac.kr>
2024-02-20 11:49:40 +0900

    :tada: begin: 프로젝트 시작

---
+++ src/main/java/com/example/beomstagram/BeomstagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.beomstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeomstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeomstagramApplication.class, args);
+	}
+
+}
