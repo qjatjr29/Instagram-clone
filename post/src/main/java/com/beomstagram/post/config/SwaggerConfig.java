980715a24c20632bdcce8fe22ca1d9bc9e09fe77
beomsic <qjatjr29@khu.ac.kr>
2024-02-25 13:24:57 +0900

    :sparkles: feat: 게시물 서비스 추가
    - Swagger 설정
    - 공통 모듈 사용을 위한 config 설정

---
+++ post/src/main/java/com/beomstagram/post/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.beomstagram.post.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Beomstagram - Post ")
+                        .description("Beomstagram Post Controller")
+                        .version("1.0.0"));
+    }
+}
