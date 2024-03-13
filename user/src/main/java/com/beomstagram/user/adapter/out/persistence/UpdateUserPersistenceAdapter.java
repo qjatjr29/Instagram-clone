7cb7543bdc0778f303bec8c2064f02ee1a7d3564
beomsic <qjatjr29@khu.ac.kr>
2024-03-14 00:21:58 +0900

    :sparkles: feat: token 갱신 기능 추가

--- user/src/main/java/com/beomstagram/user/adapter/out/persistence/UpdateUserPersistenceAdapter.java
+++ user/src/main/java/com/beomstagram/user/adapter/out/persistence/UpdateUserPersistenceAdapter.java
@@ -1,17 +1,14 @@
 package com.beomstagram.user.adapter.out.persistence;
 
 import com.beomstagram.common.annotation.PersistenceAdapter;
-import com.beomstagram.user.application.port.out.RegisterUserPort;
 import com.beomstagram.user.application.port.out.UpdateUserPort;
+import lombok.RequiredArgsConstructor;
 
 @PersistenceAdapter
+@RequiredArgsConstructor
 public class UpdateUserPersistenceAdapter implements UpdateUserPort {
 
     private final UserRepository userRepository;
-
-    public UpdateUserPersistenceAdapter(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserEntity updateUserName(Long userId, String newName) {
@@ -19,6 +16,16 @@ public class UpdateUserPersistenceAdapter implements UpdateUserPort {
                 .orElseThrow(() -> new IllegalArgumentException());
 
         userEntity.updateName(newName);
+
+        return userRepository.save(userEntity);
+    }
+
+    @Override
+    public UserEntity updateRefreshToken(Long userId, String token) {
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException());
+
+        userEntity.updateRefreshToken(token);
 
         return userRepository.save(userEntity);
     }
