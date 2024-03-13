61676aca029dd0953288b5c76492e056cf875101
beomsic <qjatjr29@khu.ac.kr>
2024-03-14 00:22:19 +0900

    :sparkles: feat: token 갱신 기능 추가

--- user/src/main/java/com/beomstagram/user/application/port/out/UpdateUserPort.java
+++ user/src/main/java/com/beomstagram/user/application/port/out/UpdateUserPort.java
@@ -4,6 +4,7 @@ import com.beomstagram.user.adapter.out.persistence.UserEntity;
 
 public interface UpdateUserPort {
     UserEntity updateUserName(Long userId, String newName);
+    UserEntity updateRefreshToken(Long userId, String token);
 //    UserEntity updateUserNickName(Long userId, String newNickname);
 //    UserEntity updateUserPassword(Long userId, String newPassword);
 //    UserEntity updateUserProfileImage(Long userId, String newProfileImage);
