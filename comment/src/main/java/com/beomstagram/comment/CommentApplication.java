a05db4af24e4bae0ca26e188022e11c09ed52d04
beomsic <qjatjr29@khu.ac.kr>
2024-03-22 20:14:46 +0900

    :sparkles: feat: @EnableNeo4jRepositories 설정

--- comment/src/main/java/com/beomstagram/comment/CommentApplication.java
+++ comment/src/main/java/com/beomstagram/comment/CommentApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableNeo4jRepositories
 public class CommentApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentApplication.class, args);
