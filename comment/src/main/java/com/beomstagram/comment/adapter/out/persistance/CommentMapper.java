337556249940508712d101aacf5e4d3de2c7fd4f
beomsic <qjatjr29@khu.ac.kr>
2024-03-22 20:43:18 +0900

    :recycle: refactor: mapper 수정

--- comment/src/main/java/com/beomstagram/comment/adapter/out/persistance/CommentMapper.java
+++ comment/src/main/java/com/beomstagram/comment/adapter/out/persistance/CommentMapper.java
@@ -1,26 +1,29 @@
 package com.beomstagram.comment.adapter.out.persistance;
 
 import com.beomstagram.comment.domain.Comment;
-import com.beomstagram.comment.domain.Reply;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentMapper {
 
+    private final ReplyMapper replyMapper;
+
+    public CommentMapper(ReplyMapper replyMapper) {
+        this.replyMapper = replyMapper;
+    }
+
     public Comment mapToDomain(CommentEntity commentEntity) {
         return Comment.generateComment(
+                commentEntity.getId(),
+                commentEntity.getPostId(),
                 commentEntity.getUserId(),
                 commentEntity.getUsername(),
                 commentEntity.getContent(),
-                commentEntity.getReplies()
-                        .stream()
-                        .map(this::mapToReplyDomain)
+                commentEntity.getReplies().stream()
+                        .map(replyMapper::mapToDomain)
                         .toList(),
+                commentEntity.getPostType(),
                 commentEntity.getCreatedAt(),
                 commentEntity.getUpdatedAt());
-    }
-
-    private Reply mapToReplyDomain(ReplyEntity replyEntity) {
-        return Reply.generateReply(replyEntity.getUsername(), replyEntity.getReply());
     }
 }
