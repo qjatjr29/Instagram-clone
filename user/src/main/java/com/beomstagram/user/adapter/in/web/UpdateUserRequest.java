59e4cfa868842f68f11571101b2cd91f81b66d63
beomsic <qjatjr29@khu.ac.kr>
2024-05-06 16:45:41 +0900

    :sparkles: feat: Null 관련 어노테이션 추가
    - profileImage 에 대한 정보도 추가

--- user/src/main/java/com/beomstagram/user/adapter/in/web/UpdateUserRequest.java
+++ user/src/main/java/com/beomstagram/user/adapter/in/web/UpdateUserRequest.java
@@ -1,12 +1,18 @@
 package com.beomstagram.user.adapter.in.web;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UpdateUserRequest {
+    @Nullable
     private String email;
+    @Nullable
     private String name;
+    @Nullable
     private String password;
+    @Nullable
+    private String profileImage;
 }
