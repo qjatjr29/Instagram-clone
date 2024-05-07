873a06686181ec47c25b32c732ae6889e934930c
beomsic <qjatjr29@khu.ac.kr>
2024-05-07 13:24:29 +0900

    :sparkles: feat: 유저 업데이트 메시지를 위한 카프카 설정

---
+++ comment/src/main/java/com/beomstagram/comment/config/KafkaConsumerConfig.java
@@ -0,0 +1,52 @@
+package com.beomstagram.comment.config;
+
+import com.beomstagram.common.event.user.UserUpdatedEventMessagePayload;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    private final String bootstrapServers;
+
+    public KafkaConsumerConfig(@Value("${kafka.clusters.bootstrapservers}") String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+    @Bean
+    public ConsumerFactory<String, UserUpdatedEventMessagePayload> consumerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        JsonDeserializer<UserUpdatedEventMessagePayload> deserializer
+                = new JsonDeserializer<>(UserUpdatedEventMessagePayload.class, false);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UserUpdatedEventMessagePayload> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, UserUpdatedEventMessagePayload> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+
+}
